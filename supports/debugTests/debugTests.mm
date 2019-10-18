5e73f1ff503b5ba0a81f5664079e6b25d6baca02
Edmond <liugc@xueqiu.com>
2019-10-18 13:42:34 +0800

    support cocoapods static framework

---
+++ supports/debugTests/debugTests.mm
@@ -0,0 +1,56 @@
+//
+//  debugTests.m
+//  debugTests
+//
+//  Created by Jeong YunWon on 12. 10. 22..
+//  Copyright (c) 2012 youknowone.org. All rights reserved.
+//
+
+
+#import <XCTest/XCTest.h>
+#import "debug.h"
+
+
+@interface debugTests : XCTestCase
+
+@end
+
+
+@implementation debugTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testCrash
+{
+    if (!_DEBUG) {
+        XCTFail(@"Not debug mode");
+    }
+    dprintf(@"Hello %@", @"World");
+    dlog(1, @"Hello %@", @"World");
+    dassert(true);
+    dassertlog(true, @"This will now shown");
+}
+
+- (void)testOutput
+{
+    dlog(1, @"-----------------------");
+    dlog(1, @"Is this formatted well? %@", @"yay!");
+    for (int i = 0; i < 3; i++) {
+        dlog(1, @"\tNumber test: %d", i);
+    }
+    dlog(1, @"-----------------------");
+}
+
+@end
